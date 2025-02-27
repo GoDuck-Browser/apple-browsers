9f352998e2f679e675f40b75833bdf230f7213d3
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-02-27 09:01:40 +0100

    Fix Package.swift

--- BrowserServicesKit/Package.swift
+++ BrowserServicesKit/Package.swift
@@ -53,7 +53,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/duckduckgo/duckduckgo-autofill.git", exact: "16.2.2"),
         .package(url: "https://github.com/duckduckgo/GRDB.swift.git", exact: "2.4.2"),
-        .package(url: "https://github.com/duckduckgo/TrackerRadarKit", exact: "3.0.0"),
+        .package(url: "https://github.com/duckduckgo/TrackerRadarKit.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/sync_crypto", exact: "0.5.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", exact: "3.0.0"),
         .package(url: "https://github.com/duckduckgo/content-scope-scripts", branch: "pr-releases/pr-1487"),
