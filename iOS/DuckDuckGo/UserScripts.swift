6ecfa6e59262592f0c1b45390d0d2f37d1d129bf
Sabrina Tardio <stardio@duckduckgo.com>
2025-03-19 16:40:53 +0100

    wip

--- iOS/DuckDuckGo/UserScripts.swift
+++ iOS/DuckDuckGo/UserScripts.swift
@@ -66,10 +66,12 @@ final class UserScripts: UserScriptsProvider {
         
         loginFormDetectionScript = sourceProvider.loginDetectionEnabled ? LoginFormDetectionUserScript() : nil
         contentScopeUserScript = ContentScopeUserScript(sourceProvider.privacyConfigurationManager,
-                                                        properties: sourceProvider.contentScopeProperties)
+                                                        properties: sourceProvider.contentScopeProperties,
+                                                        privacyConfigurationJsonGenerator: CSSPrivacyConfigurationJsonGenerator(featureFlagger: AppDependencyProvider.shared.featureFlagger, privacyConfigurationManager: sourceProvider.privacyConfigurationManager))
         contentScopeUserScriptIsolated = ContentScopeUserScript(sourceProvider.privacyConfigurationManager,
                                                                 properties: sourceProvider.contentScopeProperties,
-                                                                isIsolated: true)
+                                                                isIsolated: true,
+                                                                privacyConfigurationJsonGenerator: CSSPrivacyConfigurationJsonGenerator(featureFlagger: AppDependencyProvider.shared.featureFlagger, privacyConfigurationManager: sourceProvider.privacyConfigurationManager))
         autoconsentUserScript = AutoconsentUserScript(config: sourceProvider.privacyConfigurationManager.privacyConfig)
 
         let aiChatScriptHandler = AIChatUserScriptHandler(featureFlagger: featureFlagger)
