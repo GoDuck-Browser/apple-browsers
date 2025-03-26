5c497ea138a8ef07d231181eaf9673ed97538439
Federico Cappelli <kap.posta@gmail.com>
2025-03-26 15:33:36 +0000

    unwanted changes reverted

--- BrowserServicesKit/Sources/Subscription/Managers/SubscriptionManagerV2.swift
+++ BrowserServicesKit/Sources/Subscription/Managers/SubscriptionManagerV2.swift
@@ -138,6 +138,8 @@ public protocol SubscriptionManagerV2: SubscriptionTokenProvider, SubscriptionAu
     /// - Returns: An auth v2 TokenContainer
     func exchange(tokenV1: String) async throws -> TokenContainer
 
+    func adopt(accessToken: String, refreshToken: String) async throws
+
     /// Used only from the Mac Packet Tunnel Provider when a token is received during configuration
     func adopt(tokenContainer: TokenContainer)
 
@@ -253,7 +255,9 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
     public func loadInitialData() async {
         Logger.subscription.log("Loading initial data...")
 
-        // Fetching fresh subscription
+        await migrateAuthV1toAuthV2IfNeeded()
+
+        Logger.subscription.log("Fetching fresh subscription")
         do {
             _ = try await currentSubscriptionFeatures(forceRefresh: true)
             let subscription = try await getSubscription(cachePolicy: .returnCacheDataDontLoad)
@@ -374,8 +378,6 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
             let currentCachedTokenContainer = oAuthClient.currentTokenContainer
             let currentCachedEntitlements = currentCachedTokenContainer?.decodedAccessToken.subscriptionEntitlements
 
-            await migrateAuthV1toAuthV2IfNeeded()
-
             let resultTokenContainer = try await oAuthClient.getTokens(policy: policy)
             let newEntitlements = resultTokenContainer.decodedAccessToken.subscriptionEntitlements
 
@@ -425,6 +427,11 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
         let tokenContainer = try await oAuthClient.exchange(accessTokenV1: tokenV1)
         NotificationCenter.default.post(name: .accountDidSignIn, object: self, userInfo: nil)
         return tokenContainer
+    }
+
+    public func adopt(accessToken: String, refreshToken: String) async throws {
+        let tokenContainer = try await oAuthClient.decode(accessToken: accessToken, refreshToken: refreshToken)
+        oAuthClient.adopt(tokenContainer: tokenContainer)
     }
 
     public func adopt(tokenContainer: TokenContainer) {
